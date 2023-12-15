34aa82ac29863058e6621ae8d83046afa5c026d3
flowerfalling <zhaowenrui_8866@163.com>
2023-12-15 13:21:08 +0800

    Separate message types

---
+++ suswx/Content.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/15 10:12
+# @Author  : 之落花--falling_flowers
+# @File    : Content.py
+# @Software: PyCharm
+from enum import Enum
+
+
+class Content(Enum):
+    TEXT = 1
+    PICTURE = 3
+    AUDIO = 34
+    VIDEO = 47
+    FILE = 1090519089
