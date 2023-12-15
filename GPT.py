2684d9ead0e8674c70487c1b9a26d7cce618f284
flowerfalling <zhaowenrui_8866@163.com>
2023-12-15 13:21:39 +0800

    Change GPT and Robot

--- GPT.py
+++ GPT.py
@@ -8,112 +8,54 @@ import json
 import requests
 import wcferry
 
-import Config
-
-URL = "http://w5.xjai.cc/api/chat-process"
-SYSTEM_MESSAGE = "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown."
-HEADERS = {
-    "Host": "w5.xjai.cc",
-    "Proxy-Connection": "keep-alive",
-    "Pragma": "no-cache",
-    "Cache-Control": "no-cache",
-    "Accept": "application/json, text/plain, */*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-    "Content-Type": "application/json",
-    "Origin": "http://w5.xjai.cc",
-    "Referer": "http://w5.xjai.cc/",
-    "Accept-Encoding": "gzip, deflate",
-    "Accept-Language": "zh-CN,zh;q=0.9",
-}
-
-GPT_HELP = '''gpt command:
-  /xxx 与gpt对话
-  /gpt help 获取帮助
-  /gpt start 开启gpt连续对话
-  /gpt end 关闭gpt连续对话
-  /gpt clear 清空当前会话'''
-
-
-class GPTInfo(object):
-    def __init__(self) -> None:
-        self.__state: bool = False
-        self.__top_p: float = 1.0
-        self.__temperature: float = 0.8
-        self.pmid: str = ""
-
-    @property
-    def state(self) -> bool:
-        return self.__state
-
-    @property
-    def top_p(self) -> float:
-        return self.__top_p
-
-    @property
-    def temperature(self) -> float:
-        return self.__temperature
-
-    @top_p.setter
-    def top_p(self, value: float) -> None:
-        if value < 0 or value > 1:
-            raise ValueError("You must enter a number between 0 and 1")
-        else:
-            self.__top_p = value
-
-    @temperature.setter
-    def temperature(self, value: float) -> None:
-        if value < 0 or value > 2:
-            raise ValueError("You must enter a number between 0 and 2")
-        else:
-            self.__temperature = value
-
-    def command(self, order: str) -> str:
-        match order:
-            case "start":
-                self.__state = True
-                return "ChatGPT has been turned on"
-            case "end":
-                self.__state = False
-                return "ChatGPT has been turned off"
-            case "clear":
-                self.pmid = ""
-                return "ChatGPT has been cleared memory"
-            case "help":
-                return GPT_HELP
-            case _:
-                return '指令错误,可发送"/gpt help"获取帮助'
+from suswx import Configuration
 
 
 class GPT(object):
-    def __init__(self, wcf: wcferry.Wcf) -> None:
-        self.__wcf = wcf
-        self.__url: str = URL
-        self.__info: dict[str, GPTInfo] = {}
+    __URL = "http://w5.xjai.cc/api/chat-process"
+    __SYSTEM_MESSAGE = "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown."
+    __HEADERS = {
+        "Host": "w5.xjai.cc",
+        "Proxy-Connection": "keep-alive",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
+        "Accept": "application/json, text/plain, */*",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+        "Content-Type": "application/json",
+        "Origin": "http://w5.xjai.cc",
+        "Referer": "http://w5.xjai.cc/",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+    }
+
+    def __init__(self, wcf: wcferry.Wcf, config: Configuration) -> None:
+        self.__wcf: wcferry.Wcf = wcf
+        self.__config: Configuration = config
+        self.__info: dict[str, GPT._GPTInfo] = {}
 
     def private_reply(self, msg: wcferry.WxMsg) -> None:
         sender = msg.sender
-        if sender not in Config.GPT:
+        if sender not in self.__config.chatgpt:
             return
-        user = self.__info.setdefault(sender, GPTInfo())
+        user = self.__info.setdefault(sender, GPT._GPTInfo())
         content = msg.content
         if content.startswith("/gpt"):
             self.__wcf.send_text(user.command(content.split(" ")[-1]), sender)
             return
         if user.state or content.startswith("/"):
-            response = self.__reply(content[int(not user.state) :], user)
+            response = self.__reply(content[int(not user.state):], user)
             user.pmid = response["id"]
             self.__wcf.send_text("[GPT]%s" % response["text"], sender)
 
-    @staticmethod
-    def __reply(msg: str, info: GPTInfo) -> dict:
+    def __reply(self, msg: str, info: "GPT._GPTInfo") -> dict:
         response = json.loads(
             requests.post(
-                URL,
-                headers=HEADERS,
+                self.__URL,
+                headers=self.__HEADERS,
                 json={
                     "prompt": msg,
                     "options": {"parentMessageId": info.pmid} if info.pmid else {},
-                    "systemMessage": SYSTEM_MESSAGE,
+                    "systemMessage": self.__SYSTEM_MESSAGE,
                     "temperature": info.temperature,
                     "top_p": info.top_p,
                 },
@@ -121,3 +63,58 @@ class GPT(object):
         )
         return {"id": response["id"], "text": response["text"]}
 
+    class _GPTInfo(object):
+        __GPT_HELP = '''gpt command:
+  /xxx 与gpt对话
+  /gpt help 获取帮助
+  /gpt start 开启gpt连续对话
+  /gpt end 关闭gpt连续对话
+  /gpt clear 清空当前会话'''
+
+        def __init__(self) -> None:
+            self.__state: bool = False
+            self.__top_p: float = 1.0
+            self.__temperature: float = 0.8
+            self.pmid: str = ""
+
+        @property
+        def state(self) -> bool:
+            return self.__state
+
+        @property
+        def top_p(self) -> float:
+            return self.__top_p
+
+        @property
+        def temperature(self) -> float:
+            return self.__temperature
+
+        @top_p.setter
+        def top_p(self, value: float) -> None:
+            if value < 0 or value > 1:
+                raise ValueError("You must enter a number between 0 and 1")
+            else:
+                self.__top_p = value
+
+        @temperature.setter
+        def temperature(self, value: float) -> None:
+            if value < 0 or value > 2:
+                raise ValueError("You must enter a number between 0 and 2")
+            else:
+                self.__temperature = value
+
+        def command(self, order: str) -> str:
+            match order:
+                case "start":
+                    self.__state = True
+                    return "ChatGPT has been turned on"
+                case "end":
+                    self.__state = False
+                    return "ChatGPT has been turned off"
+                case "clear":
+                    self.pmid = ""
+                    return "ChatGPT has been cleared memory"
+                case "help":
+                    return self.__GPT_HELP
+                case _:
+                    return '指令错误,可发送"/gpt help"获取帮助'
--- suswx/Robot.py
+++ suswx/Robot.py
@@ -3,14 +3,15 @@
 # @Author  : 之落花--falling_flowers
 # @File    : Robot.py
 # @Software: PyCharm
+import atexit
 import queue
 import time
-from enum import Enum
 from typing import Callable
 
 import wcferry
 from wcferry import Wcf
-from GPT import GPT
+
+from suswx.Content import Content
 
 
 class Robot(object):
@@ -19,20 +20,16 @@ class Robot(object):
         self.registry: dict = dict.fromkeys((0, 1, 3, 37, 47, 1090519089), ([], []))  # (私聊, 组群)
 
     def run(self) -> None:
-        while self.wcf.enable_receiving_msg():
+        self.wcf.enable_receiving_msg()
+        while self.wcf.is_receiving_msg():
             time.sleep(0.5)
             try:
                 msg = self.wcf.get_msg()
                 if msg.from_self() and msg.content == "/quit":
                     break
-                try:
-                    self.process(msg)
-                except:
-                    break
                 self.process(msg)
             except queue.Empty:
                 continue
-        self.quit()
 
     def process(self, msg: wcferry.WxMsg) -> None:
         from_group = int(msg.from_group())
@@ -45,9 +42,9 @@ class Robot(object):
                 i(msg)
 
     def register(
-        self,
+            self,
             func: Callable,
-            msgType: "Robot.Content",
+            msgType: Content,
             fromGroup: bool = False,
             fromFriend: bool = False,
     ) -> None:
@@ -59,23 +56,7 @@ class Robot(object):
     def register_command(self, func: Callable) -> None:
         self.registry["me"].append(func)
 
+    @atexit.register
     def quit(self) -> None:
         self.wcf.cleanup()
-
-    class Content(Enum):
-        TEXT = 1
-        PICTURE = 3
-        AUDIO = 34
-        VIDEO = 47
-        FILE = 1090519089
-
-
-def main():
-    wcf = Wcf(debug=True)
-    robot = Robot(wcf)
-    robot.register(GPT(wcf).private_reply, Robot.Content.TEXT, False, True)
-    robot.run()
-
-
-if __name__ == "__main__":
-    main()
+        print('Quit done')
