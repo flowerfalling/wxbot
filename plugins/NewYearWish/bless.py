f9bcd9edabb468bc7ce0147798d792aeff963367
flowerfalling <zhaowenrui_8866@163.com>
2024-02-10 10:28:45 +0800

    Add reply delay for NewYearWish

--- plugins/NewYearWish/bless.py
+++ plugins/NewYearWish/bless.py
@@ -3,23 +3,26 @@
 # @Author  : 之落花--falling_flowers
 # @File    : bless.py
 # @Software: PyCharm
+import asyncio
 import random
 
 import wcferry
 
-from plugins.NewYearWish.blessings import blessings
 from plugins import register
+from plugins.NewYearWish.blessings import blessings
 from suswx.common import wcf, logger
 
 
 def check(msg: wcferry.WxMsg) -> bool:
-    for c in ("新年", "快乐", "春节", "安康", "除夕", "龙年", "过年", "吉祥", "开心", "祝", "福", "新的一年", 'happy', 'year'):
+    for c in (
+    "新年", "快乐", "春节", "安康", "除夕", "龙年", "过年", "吉祥", "开心", "祝", "福", "新的一年", 'happy', 'year'):
         if c in msg.content.lower():
             return True
     return False
 
 
-@register(fromFriend=True, access={"ALL"}, check=[check])
-def bless(msg: wcferry.WxMsg) -> None:
+@register(fromFriend=True, access={"ALL"}, mode="async", check=[check])
+async def bless(msg: wcferry.WxMsg) -> None:
+    await asyncio.sleep(1)
     wcf.send_text(info := random.choice(blessings), msg.sender)
     logger.info(info)
